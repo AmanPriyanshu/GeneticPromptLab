f36d86289785d284333c6c8b3e9bcd480d40cf40
AmanPriyanshu <aman@robustintelligence.com>
2024-06-19 21:25:44 -0700

    redoing main class

--- main.py
+++ main.py
@@ -25,23 +25,30 @@ def send_query2gpt(client, messages, function_template):
     return generated_response
 
 class GeneticPromptLab:
-    def __init__(self, train_path, test_path, model_name, sample_p=1.0, init_and_fitness_sample=10):
+    def __init__(self, train_questions_list, train_answers_label, test_questions_list, test_answers_label, label_dict, model_name, sample_p=1.0, init_and_fitness_sample=10):
         self.init_and_fitness_sample = init_and_fitness_sample
-        self.train_data = pd.read_csv(train_path)
-        self.test_data = pd.read_csv(test_path)
+        self.test_questions_list = test_questions_list
+        self.test_answers_label = test_answers_label
+        self.label_dict = label_dict
+
         self.model = SentenceTransformer(model_name)
         self.sample_p = sample_p
-        self.train_data = self.train_data.sample(frac=self.sample_p)
-        self.embeddings = self.model.encode(self.train_data['question'].tolist(), show_progress_bar=True)
+        train_indices_list = np.random.choice(np.arange(len(train_questions_list)), size=int(len(train_questions_list)*self.sample_p))
+        self.train_questions_list = [train_questions_list[i] for i in train_indices_list]
+        self.train_answers_label = [train_answers_label[i] for i in train_indices_list]
+        self.embeddings = self.model.encode(self.train_questions_list, show_progress_bar=True)
         self.already_sampled_indices = set()
 
-    def generate_init_prompts(self, N):
-        subset_n = N//self.init_and_fitness_sample
+    def generate_init_prompts(self):
         distinct_samples = self.sample_distinct(self.init_and_fitness_sample)
+        for sample_index in distinct_samples:
+            question = self.train_questions_list[int(sample_index)]
+            answer = self.train_answers_label[int(sample_index)]
+            print(question, answer)
         ## FILL HERE
+        exit()
 
     def sample_distinct(self, n):
-        num_samples = int(len(self.train_data) * self.sample_p)
         embeddings = self.embeddings
 
         if len(self.already_sampled_indices) > 0:
@@ -64,8 +71,8 @@ class GeneticPromptLab:
         self.already_sampled_indices.update(sampled_indices)
         return sampled_indices
 
-    def genetic_algorithm(self, population_size, generations, mutation_rate=0.1):
-        initial_prompts = self.generate_init_prompts(population_size)
+    def genetic_algorithm(self, generations, mutation_rate=0.1):
+        initial_prompts = self.generate_init_prompts()
         population = initial_prompts
 
         for _ in range(generations):
@@ -118,11 +125,19 @@ if __name__ == '__main__':
     test_path = './data/ag_news_test.csv'
     model_name = 'multi-qa-MiniLM-L6-cos-v1'
     sample_p = 0.01
+    train_data = pd.read_csv(train_path)
+    test_data = pd.read_csv(test_path)
+    with open("./data/ag_news_label_dict.json", "r") as f:
+        label_dict = json.load(f)
+        label_dict = {i:v for i,v in enumerate(label_dict)}
 
+    train_questions_list, train_answers_label, test_questions_list, test_answers_label = train_data['question'].tolist(), train_data['label'].tolist(), test_data['question'].tolist(), test_data['label'].tolist()
     # Create GeneticPromptLab instance
-    lab = GeneticPromptLab(train_path, test_path, model_name, sample_p)
-    population_size = 100
+
+    population_size = 10
     generations = 10
-    optimized_prompts = lab.genetic_algorithm(population_size, generations)
+
+    lab = GeneticPromptLab(train_questions_list, train_answers_label, test_questions_list, test_answers_label, label_dict, model_name, sample_p, init_and_fitness_sample=population_size)
+    optimized_prompts = lab.genetic_algorithm(generations)
 
     print("Optimized Prompts:", optimized_prompts)
